fa658e4d195df341486fd238e1be40981579451a
alex-rns <alexrns3@gmail.com>
2018-05-25 17:35:27 +0300

    add arrow for slider

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -49,23 +49,42 @@ const dataSlider = [
 ];
 
 
+function SampleNextArrow(props) {
+  const { className, onClick } = props;
+  return (
+    <div
+      className={className}
+      onClick={onClick}
+    />
+  );
+}
+
+function SamplePrevArrow(props) {
+  const { className, onClick } = props;
+  return (
+    <div
+      className={className}
+      onClick={onClick}
+    />
+  );
+}
+
 class Carousel extends React.Component {
   render() {
     let settings = {
-      dots: true,
       infinite: true,
       speed: 500,
       slidesToShow: 3,
       slidesToScroll: 1,
-      arrows: false,
+      nextArrow: <SampleNextArrow />,
+      prevArrow: <SamplePrevArrow />,
       responsive: [
         {
           breakpoint: 1024,
           settings: {
             slidesToShow: 2,
             slidesToScroll: 1,
-            infinite: true,
-            dots: true
+            infinite: true
           }
         }
       ]
